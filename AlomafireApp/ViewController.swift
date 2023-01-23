3572f68b9f9a15ac93ef4d218f06448c4cbe6ddf
Md Abrar Nasim <abrarnsim555@gmail.com>
2023-01-23 17:40:12 +0530

    First commit

--- AlomafireApp/ViewController.swift
+++ AlomafireApp/ViewController.swift
@@ -6,14 +6,23 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var TextView1: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func Button1(_ sender: UIButton) {
+        TextView1.text = "Button was clicked";
+        AF.request("https://reqres.in/api/users/2").response{
+            response in debugPrint(response)
+        }
+    }
+    
 }
 
